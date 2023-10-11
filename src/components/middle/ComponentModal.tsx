b2e636ab2e9d67b9e07f8c84f5089a57c179d0d0
Laura Ramirez <lauraramirez@Lauras-MacBook-Air.local>
2023-10-11 12:14:08 -0400

    new change

--- src/components/middle/ComponentModal.tsx
+++ src/components/middle/ComponentModal.tsx
@@ -1,23 +1,23 @@
-// import {
-//   Dialog,
-//   DialogTitle,
-//   DialogContent,
-//   DialogContentText,
-//   DialogActions,
-//   TextField,
-// } from '@mui/material';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from '@mui/material';
 
-// function ComponentModal(props) {
-//   return (
-//     <>
-//       <Dialog>
-//         <DialogTitle>New Component</DialogTitle>
-//         <DialogContent>
-//           <DialogContentText></DialogContentText>
-//         </DialogContent>
-//       </Dialog>
-//     </>
-//   );
-// }
+function ComponentModal(props) {
+  return (
+    <>
+      <Dialog>
+        <DialogTitle>New Component</DialogTitle>
+        <DialogContent>
+          <DialogContentText></DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
 
-// export default ComponentModal;
+export default ComponentModal;
