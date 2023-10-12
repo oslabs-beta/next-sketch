ebfc435b8a54474c003bd01f19241fd46b1b5a2a
James Huang <jhuang4647@gmail.com>
2023-10-12 10:37:10 -0400

    implemented drag and drop functioanlity

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx';
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
 
