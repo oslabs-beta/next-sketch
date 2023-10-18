3ce66be3fde1fab3ee4da9746bd02532c186f8b1
Laura Ramirez <lauraramirez@Lauras-MacBook-Air.local>
2023-10-18 18:05:08 -0400

    delete button modifies the code preview

--- src/components/middle/DeleteBtn.tsx
+++ src/components/middle/DeleteBtn.tsx
@@ -1,7 +1,8 @@
-import React, { MouseEvent, useContext } from "react";
-import { Button } from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
-import { Context } from "./CreateComponentBtn";
+import React, { MouseEvent, useContext } from 'react';
+import { Button } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import { Context } from './CreateComponentBtn';
+import { CodeContext } from '../../App';
 
 interface DeleteBtnProps {
   id: number;
@@ -9,6 +10,8 @@ interface DeleteBtnProps {
 
 function DeleteBtn({ id }: DeleteBtnProps) {
   const [listComponents, setListComponents] = useContext(Context);
+  //modify code when a component is deleted
+  const [componentName, setComponentName] = useContext(CodeContext);
 
   const handleRemoveClick = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -16,12 +19,13 @@ function DeleteBtn({ id }: DeleteBtnProps) {
     setListComponents(
       listComponents.filter((component, index) => index !== deletedComponent)
     );
+    setComponentName(listComponents[deletedComponent - 1]);
   };
   return (
     <>
       <Button
-        color="error"
-        size="small"
+        color='error'
+        size='small'
         sx={{ minWidth: 24, minHeight: 24, padding: 0 }}
         onClick={handleRemoveClick}
       >
