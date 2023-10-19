fc76cc70307959863553e279667b3b08fbb6875f
James Huang <jhuang4647@gmail.com>
2023-10-19 12:03:23 -0400

    most recent update

--- src/components/left/CustomEndpoint.tsx
+++ src/components/left/CustomEndpoint.tsx
@@ -87,118 +87,108 @@ const CustomEndpoint = ({
                 alert('Please enter a file name')
             }
     };
-
-<!--   return (
-    <div className='cursor'>
-      <form>
-        <input
-          type='text'
-          autoFocus
-          placeholder=' New Endpoint'
-          onChange={handleChange}
-          value={inputValue}
-        />
-
-        <button type='submit' onClick={handleCreateCustomFolder}>
-          Submit
-        </button>
-      </form>
-
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-title'
-        aria-describedby='modal-description'
-      >
-        <Box sx={style}>
-          <Typography
-            id='modal-title'
-            variant='h6'
-            component='h2'
-            style={{ marginBottom: 20, fontSize: 30 }}
-          >
-            Choose Your Template Files
-          </Typography>
-
-          <div>
-            <Checkbox
-              name='default.tsx'
-              checked={selectedItems.default}
-              onChange={handleModalChange}
-            />
-            default.tsx
-          </div>
-
-          <div>
-            <Checkbox
-              name='error.tsx'
-              checked={selectedItems.error}
-              onChange={handleModalChange}
-            />
-            error.tsx
-          </div>
-
-          <div>
-            <Checkbox
-              name='layout.tsx'
-              checked={selectedItems.layout}
-              onChange={handleModalChange} -->
     return (
-        <div className='cursor' >
-            <form >
-            <input 
-            type="text"
-            autoFocus
-            placeholder= " New Endpoint"
-            onChange ={handleChange}
-            value={inputValue}
+        <div className='cursor'>
+          <form>
+            <input
+              type='text'
+              autoFocus
+              placeholder=' New Endpoint'
+              onChange={handleChange}
+              value={inputValue}
             />
-            layout.tsx
-          </div>
-
-          <div>
-            <Checkbox
-              name='loading.tsx'
-              checked={selectedItems.loading}
-              onChange={handleModalChange}
-            />
-            loading.tsx
-          </div>
-
-          <div>
-            <Checkbox
-              name='not-found.tsx'
-              checked={selectedItems.notFound}
-              onChange={handleModalChange}
-            />
-            not-found.tsx
-          </div>
-
-          <div>
-            <Checkbox
-              name='route.tsx'
-              checked={selectedItems.route}
-              onChange={handleModalChange}
-            />
-            route.tsx
-          </div>
-
-          <div>
-            <Checkbox
-              name='template.tsx'
-              checked={selectedItems.template}
-              onChange={handleModalChange}
-            />
-            template.tsx
-          </div>
-
-          <Button onClick={handleClose} sx={{ mt: 3 }}>
-            Submit
-          </Button>
-        </Box>
-      </Modal>
-    </div>
-  );
+    
+            <button type='submit' onClick={handleCreateCustomFolder}>
+              Submit
+            </button>
+          </form>
+    
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby='modal-title'
+            aria-describedby='modal-description'
+          >
+            <Box sx={style}>
+              <Typography
+                id='modal-title'
+                variant='h6'
+                component='h2'
+                style={{ marginBottom: 20, fontSize: 30 }}
+              >
+                Choose Your Template Files
+              </Typography>
+    
+              <div>
+                <Checkbox
+                  name='default.tsx'
+                  checked={selectedItems.default}
+                  onChange={handleModalChange}
+                />
+                default.tsx
+              </div>
+    
+              <div>
+                <Checkbox
+                  name='error.tsx'
+                  checked={selectedItems.error}
+                  onChange={handleModalChange}
+                />
+                error.tsx
+              </div>
+    
+              <div>
+                <Checkbox
+                  name='layout.tsx'
+                  checked={selectedItems.layout}
+                  onChange={handleModalChange}
+                />
+                layout.tsx
+              </div>
+    
+              <div>
+                <Checkbox
+                  name='loading.tsx'
+                  checked={selectedItems.loading}
+                  onChange={handleModalChange}
+                />
+                loading.tsx
+              </div>
+    
+              <div>
+                <Checkbox
+                  name='not-found.tsx'
+                  checked={selectedItems.notFound}
+                  onChange={handleModalChange}
+                />
+                not-found.tsx
+              </div>
+    
+              <div>
+                <Checkbox
+                  name='route.tsx'
+                  checked={selectedItems.route}
+                  onChange={handleModalChange}
+                />
+                route.tsx
+              </div>
+    
+              <div>
+                <Checkbox
+                  name='template.tsx'
+                  checked={selectedItems.template}
+                  onChange={handleModalChange}
+                />
+                template.tsx
+              </div>
+    
+              <Button onClick={handleClose} sx={{ mt: 3 }}>
+                Submit
+              </Button>
+            </Box>
+          </Modal>
+        </div>
+      );
 };
 
 export default CustomEndpoint;
