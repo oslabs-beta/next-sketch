467b6feb7e020b42e608d4a7caa8c84277a6bc5a
James Huang <jhuang4647@gmail.com>
2023-11-02 14:09:34 -0400

    test-4

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 const cors = require('cors');
-const PORT = 3000;
+const PORT =  process.env.PORT ||3000;
 app.use(cors());
 const fileController = require('./fileController.js');
 const archiver = require('archiver');
